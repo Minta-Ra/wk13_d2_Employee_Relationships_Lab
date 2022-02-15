a18da6d4499641254600d69092abf5554fe6a054
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-02-15 10:40:20 +0000

    Initial commit

---
+++ src/main/java/com/codeclan/example/employee_relationships_lab/EmployeeRelationshipsLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employee_relationships_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRelationshipsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRelationshipsLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/employee_relationships_lab/EmployeeRelationshipsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employee_relationships_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRelationshipsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
